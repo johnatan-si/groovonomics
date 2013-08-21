9e494991a6b038d2bc1379966c96101c289ed664
carlos <carlosgsouza@gmail.com>
2013-08-20 21:39:39 -0300

    Getting it more robust
    - Using AmazonS3Client instead of s3cmd
    - Creating S3 and SNS clients
    - Deleting messages after they are processed

--- scripts/carlosgsouza/groovonomics/typing_usage_2/AnalyzeTypingUsage.groovy
+++ scripts/carlosgsouza/groovonomics/typing_usage_2/AnalyzeTypingUsage.groovy
@@ -1,3 +1,6 @@
+import java.text.SimpleDateFormat
+
+import org.apache.commons.lang.exception.ExceptionUtils;
 import org.apache.log4j.Level
 import org.apache.log4j.LogManager
 import org.apache.log4j.Logger
@@ -5,36 +8,94 @@ import org.apache.log4j.Logger
 import carlosgsouza.groovonomics.typing_usage.ProjectInspector
 
 import com.amazonaws.auth.PropertiesCredentials
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.sns.AmazonSNSClient
+import com.amazonaws.services.sns.model.PublishRequest
 import com.amazonaws.services.sqs.AmazonSQSClient
 import com.amazonaws.services.sqs.model.DeleteMessageRequest
 import com.amazonaws.services.sqs.model.Message
 import com.amazonaws.services.sqs.model.ReceiveMessageRequest
 
-def SCRIPT_DIR = "/opt/groovonomics/src/scripts/carlosgsouza/groovonomics/typing_usage_2"
 def TEMP_DIR = "/opt/groovonomics/temp"
 
 turnOffAnnoyingLogs()
 
 def sqs = new SQS()
+def sns = new SNS()
+def s3  = new S3()
+def fs  = new FileSystem()
 
-Message msg = sqs.nextMessage
-def projectId = msg.body
+def projectId
 
-println "STARTED TO PROCESS PROJECT $projectId"
+try {
+	projectId = null
+	
+	Message msg = sqs.nextMessage
+	projectId = msg.body
+	
+	println "STARTED TO PROCESS PROJECT $projectId"
+	
+	fs.cleanWorkDir()
+	s3.downloadSource(projectId)
+	fs.unzipSource(projectId)
+	
+	def typeData = new ProjectInspector(new File("$TEMP_DIR/$projectId/")).getTypeSystemUsageData()
+	
+	def w = new File("$TEMP_DIR/${projectId}.json").newWriter()
+	w << typeData
+	w.close()
+	
+	s3.uploadResult(projectId)
+	sqs.deleteMessage(msg)
+	 
+	println "FINISHED TO PROCESS PROJECT $projectId"
+} catch(e) {
+	sns.log("ERROR", projectId, ExceptionUtils.getStackTrace(e))
+	e.printStackTrace()
+}
 
-exec "$SCRIPT_DIR/download_and_extract_project.sh $projectId"
+class FileSystem {
+	
+	def SCRIPT_DIR = "/opt/groovonomics/src/scripts/carlosgsouza/groovonomics/typing_usage_2"
+	
+	def cleanWorkDir() {
+		def workDir = new File("/opt/groovonomics/temp")
+		workDir.deleteDir()
+		workDir.mkdirs()
+	}
+	
+	def unzipSource(projectId) {
+		exec "$SCRIPT_DIR/download_and_extract_project.sh $projectId"
+	}
+	
+	
+	def exec(cmd) {
+		def proc = cmd.execute()
+		proc.consumeProcessOutput(System.out, System.err)
+		proc.waitForOrKill(3600000)
+	}
+	
+}
 
-def typeData = new ProjectInspector(new File("$TEMP_DIR/$projectId/")).getTypeSystemUsageData()
-
-def w = new File("$TEMP_DIR/${projectId}.json").newWriter()
-w << typeData
-w.close()
-
-exec "$SCRIPT_DIR/upload_results.sh"
-
-sqs.deleteMessage(msg)
- 
-println "FINISHED TO PROCESS PROJECT $projectId"
+class S3 {
+	def credentials = new PropertiesCredentials(new File("/opt/groovonomics/conf/aws.properties"))
+	def scriptStart = new Date()
+	
+	AmazonS3Client s3 = new AmazonS3Client(credentials)
+	
+	def uploadResult(projectId) {
+		s3.putObject("carlosgsouza.groovonomics", "data/type_usage/class/${projectId}.json", new File("/opt/groovonomics/temp/${projectId}.json"))
+	}
+	
+	def downloadSource(projectId) {
+		def object = s3.getObject("carlosgsouza.groovonomics", "dataset/projects/source/${projectId}.zip")
+		
+		def w = new File("/opt/groovonomics/temp/${projectId}.zip").newWriter()
+		w << object.objectContent
+		w.close()
+	}
+}
 
 class SQS {
 	def credentials = new PropertiesCredentials(new File("/opt/groovonomics/conf/aws.properties"))
@@ -51,7 +112,29 @@ class SQS {
 	}
 	
 	def deleteMessage(Message msg) {
-		new DeleteMessageRequest(queueUrl, msg.receiptHandle)
+		def deleteReq = new DeleteMessageRequest(queueUrl, msg.receiptHandle)
+		sqs.deleteMessage(deleteReq)
+	}
+}
+
+class SNS {
+	def credentials = new PropertiesCredentials(new File("/opt/groovonomics/conf/aws.properties"))
+	def topicArn = "arn:aws:sns:us-east-1:525294860386:groovonomics-log"
+	
+	def sdf = new SimpleDateFormat()
+	
+	AmazonSNSClient sns = new AmazonSNSClient(credentials)
+	
+	def now() {
+		sdf.format(new Date())
+	}
+	
+	def log(type, projectId, message) {
+		def subject = "$type | ${now()} | " + (projectId ?: "unknown project")
+		
+		def publishRequest = new PublishRequest(topicArn, message, subject)
+		
+		sns.publish(publishRequest)
 	}
 }
 
@@ -62,10 +145,4 @@ def turnOffAnnoyingLogs() {
 	for ( Logger logger : loggers ) {
 		logger.setLevel(Level.OFF);
 	}
-}
-
-def exec(cmd) {
-	def proc = cmd.execute()
-	proc.consumeProcessOutput(System.out, System.err)
-	proc.waitForOrKill(3600000)
 }
--- scripts/carlosgsouza/groovonomics/typing_usage_2/upload_results.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-s3cmd put --config=/opt/groovonomics/s3cfg /opt/groovonomics/temp/*.json s3://carlosgsouza.groovonomics/data/type_usage/class/ --force
