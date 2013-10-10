54ec7247541b1b789fd0d977ca35c75d61d5c665
carlosgsouza <carlosgsouza@gmail.com>
2013-10-10 18:54:48 -0300

    Counting commits and filtering mature projects

---
+++ src/main/groovy/carlosgsouza/groovonomics/dataset/FilterMatureProjects.groovy
@@ -0,0 +1,40 @@
+import static groovyx.net.http.ContentType.*
+import static groovyx.net.http.Method.*
+
+import java.util.concurrent.Executors;
+
+import groovy.json.JsonSlurper
+import groovyx.net.http.*
+
+
+class FilterMatureProjects {
+	def baseFolder = new File("/opt/groovonomics/")
+	def projectFolder = new File(baseFolder, "dataset/projects/metadata2")
+	def outputFile = new File(baseFolder, "dataset/projects/mature_projects.txt")
+	
+	def dataFolder = new File("/Users/carlosgsouza/workspace_gg/groovonomics/articles/aosd_2014/data/type_usage/classes")
+	
+	def wakeUp() {
+		def projects = [:]
+		
+		def pool = Executors.newFixedThreadPool(5)
+		projectFolder.eachFile { projectFile ->
+			projects[projectFile.name] = new JsonSlurper().parseText(projectFile.text)
+		}
+		
+		def matureProjects = projects.findAll{ it.value.loc.toInteger() > 2000 && it.value.commits.toInteger() > 100 && new File(dataFolder, it.key).exists()}.collect{it.key}
+		
+		if(outputFile.exists()) {
+			outputFile.delete()
+		}
+		matureProjects.each {
+			outputFile << "$it\n"
+		}
+		
+		println matureProjects.size()
+	}
+	
+	public static void main(String[] args) {
+		new FilterMatureProjects().wakeUp()
+	}
+}
