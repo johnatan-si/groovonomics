80ced7012b0de1a2439f969da37c743c17d6576c
carlosgsouza <carlosgsouza@gmail.com>
2014-01-07 00:39:45 -0200

    Adding declaration type without visibility to parsed results

--- aosd_2014/src/data-parser/src/main/groovy/carlosgsouza/groovonomics/data/DeclarationType.groovy
+++ aosd_2014/src/data-parser/src/main/groovy/carlosgsouza/groovonomics/data/DeclarationType.groovy
@@ -11,13 +11,11 @@ class DeclarationType {
 	
 	File rawDataFolder = new File("../../analysis/data/type_usage/project/")
 	File metadataFolder = new File("../../analysis/data/dataset/metadata")
-	File outputFile = new File("../../analysis/result/declaration_by_type.txt")
+	File outputFile = new File("../../analysis/parsed/declaration_by_type.txt")
 	
 	JsonSlurper slurper = new JsonSlurper()
 	
-	def DECLARATION_TYPES = [
-								"localVariable",
-								
+	def DECLARATION_TYPES_AND_VISIBILITY = [
 								"privateMethodReturn",
 								"protectedMethodReturn",
 								"publicMethodReturn",
@@ -35,16 +33,20 @@ class DeclarationType {
 //								"protectedConstructorParameter",
 //								"publicConstructorParameter"
 							]
+	
+	def DECLARATION_TYPES_WITHOUT_VISIBILITY = [
+			"localVariable", "methodReturn", "methodParameter"
+		]
 
 	def run() {
 		def result = []
 		
-		result << ["project_id", "loc"] + DECLARATION_TYPES
+		result << ["project_id", "loc"] + DECLARATION_TYPES_WITHOUT_VISIBILITY + DECLARATION_TYPES_AND_VISIBILITY
 		
 		rawDataFolder.eachFile { file ->
 			def projectId = file.name - ".json"
 			
-			result << [projectId, getLOC(projectId)] + getTypeUsage(file)  	
+			result << [projectId, getLOC(projectId)] + getTypeUsageWithoutVisibility(file) + getTypeUsage(file)  	
 		}
 		
 		if(outputFile.exists()) {
@@ -55,6 +57,21 @@ class DeclarationType {
 			outputFile << it.join("\t") + "\n"
 		}
 	}
+
+	private getTypeUsageWithoutVisibility(File file) {
+		def data = slurper.parseText(file.text)
+		
+		return DECLARATION_TYPES_WITHOUT_VISIBILITY.collect{ agregateDeclarationsOfType(it, data) }
+	}
+	
+	def agregateDeclarationsOfType(type, data) {
+		def declarationsOfType = data.findAll{ it.key.toLowerCase().endsWith(type.toLowerCase()) }*.value
+		
+		def totalS = declarationsOfType.inject(0) { acc, val -> acc + val.s.toDouble() }
+		def totalD = declarationsOfType.inject(0) { acc, val -> acc + val.d.toDouble() }
+		
+		return (totalS + totalD == 0) ? "NA" : totalS/(totalS + totalD)
+	}
 	
 	private getLOC(projectId) {
 		def metadata = slurper.parseText(new File(metadataFolder, "${projectId}.json").text)
@@ -63,7 +80,7 @@ class DeclarationType {
 
 	private getTypeUsage(File file) {
 		def data = slurper.parseText(file.text)
-		return DECLARATION_TYPES.collect{ (data[it].DRelative + data[it].DRelative) > 0 ? data[it].SRelative : "NA" }
+		return DECLARATION_TYPES_AND_VISIBILITY.collect{ (data[it].DRelative + data[it].DRelative) > 0 ? data[it].SRelative : "NA" }
 	}
 	
 	public static void main(args) {
